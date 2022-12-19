16822ac0324c069e3dbf794eb10dfb6fc8a2fbc5
Aleksandr <110767872+KeshiroXTK@users.noreply.github.com>
2022-12-19 20:31:21 +0500

    Create Interface1.cs

---
+++ HomeWork/HomeWork.Utils/Interface1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.Utils
+{
+    internal interface Interface1
+    {
+       public static void PrintInfo(int homeworkNumber, string fio)
+        {
+            Console.WriteLine($"Домашняя работа. Урок {homeworkNumber}");
+            Console.WriteLine($"Студент: {fio}");
+            Console.WriteLine("============================================");
+            Console.WriteLine();
+        }
+
+    }
+}
